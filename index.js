b70e0e47e259b13d6ea0060cc6de082ba9b06fd5
Shazil Arif <shazilarif94@gmail.com>
2020-02-11 21:53:33 -0500

    add html response and requests counts on main page

--- index.js
+++ index.js
@@ -12,11 +12,29 @@ EQ works product role work sample/challenge
 API Rate Limiting
 */
 
-let queued =  []; //here we will queue in users who exceed the request limit
+let queued =  []; //here we will queue in users who exceed the request limit and unqueue them in a first in first out manner
 let requests = {}; //quick look up for ip address and the number of requests that address made
-const MAX_REQ = 8; //max reqeuests, arbitrary number
-const TIMEOUT = 30000; //30 second timeout, arbitrary
 
+
+/* The idea is the following, requests object will look like
+requests = {
+  "/events/hourly":{
+    a mapping between ip address and number of requests made
+    "192.1.21.90":5,
+    "192.1.31.13":8
+  },
+  "/events/daily":{
+    "192.1.21.90":1,
+    "192.1.31.13":1
+  }
+}
+*/
+
+const MAX_REQ = 8; //max reqeuests, arbitrary number to simulate a limit
+const TIMEOUT = 30000; //30 second timeout, arbitrary number to simulate 
+
+
+//connect to postgres database
 const pool = new pg.Pool({
   user: 'readonly',
   host: config.get('host'), 
@@ -34,34 +52,23 @@ const queryHandler = (req, res, next) => {
 
 app.use(compression());
 
-const free_user = (route,ip) => setTimeout(free_user_helper,TIMEOUT)
+//call free_user that will call helper/axuliary function with a timeout of 30 seconds
+const free_user = () => setTimeout(free_user_helper,TIMEOUT)
 
-const free_user_helper = (route,ip) =>{
+//helper/auxiliary function to unqeue users who have been blocked from making requests
+const free_user_helper = () =>{
   let to_free = queued.shift() //following a First in first out manner, unblock the first user
   requests[to_free.endpoint][to_free.ip]= 0; //reset users request count to 0
 }
 
-/*
-requests = {
-  "/events/hourly":{
-    a mapping between ip address and number of requests made
-    "192.1.21.90":5,
-    "192.1.31.13":8
-  },
-  "/events/daily":{
-    "192.1.21.90":1,
-    "192.1.31.13":1
-  }
-}
-*/
-
+//middleware
 const rate_limiter = (req,res,next) =>{
 
   let calling_url = req.url;
   let request_ip = req.connection.remoteAddress;
   let in_queue = requests[calling_url];
-  let x = `${request_ip}`
-  console.log(requests);
+  
+  console.log(requests); //for debugging track the requests of each endpoint at any time
 
   if(in_queue && requests[calling_url][request_ip] >= MAX_REQ){
 
@@ -74,7 +81,7 @@ const rate_limiter = (req,res,next) =>{
     queued.push(next_in_queue);
 
     process.nextTick(free_user); //schedule users to be free'd immediately after
-    return res.status(429).send(`${MAX_REQ} Requests limit exceeded. Please wait 30 seconds. you can still visit other http endpoints, each has its own ${MAX_REQ} limit`)
+    return res.status(429).send(`${MAX_REQ} Requests limit exceeded. Please wait 30 seconds. you can still visit other http endpoints, each has its own ${MAX_REQ} request limit`)
   }
   if(in_queue){
     requests[calling_url][request_ip]++; //increase count by one if user already requested before
@@ -91,7 +98,33 @@ const rate_limiter = (req,res,next) =>{
 app.use(rate_limiter) //use our middleware above^, will apply to all the routes
 
 app.get('/',(req, res) => {
-  res.send('Welcome to EQ Works 😎')
+
+  /*only for demo purposes, in practice, the html responses should be kept in a seperate file*/
+
+  //get the keys of requests object which are the routes
+  let routes = Object.keys(requests);
+  let response = ``;
+
+  //iterate over each route
+  for(let i = 0; i < routes.length; i++){
+
+    //get the current request ip
+    const user_ip = req.connection.remoteAddress
+
+    //get current users request count for i'th route
+    if(user_ip in requests[routes[i]]){
+      let name = routes[i];
+      let count = requests[routes[i]][user_ip];
+
+      //build a html reponse 
+      if(count >= MAX_REQ)
+        response += `<li> route: ${name} --  requests made: ${count}, exceeded - waiting to reset <li>`;
+      else
+        response += `<li> route: ${name} --  requests made: ${count}<li>`;
+    }
+  }
+  //send the response to the client
+  return res.send(` Welcome to EQ Works 😎 above are the endpoints and how many requests you've made to each, source code: https://github.com/shazil-arif/eq-work-sample  ${response}`)
 })
 
 
@@ -147,6 +180,9 @@ app.get('/poi', (req, res, next) => {
   `
   return next()
 }, queryHandler)
+
+//deal with invalid requests
+app.get("*",(req,res)=>res.redirect('/'))
 
 app.listen(process.env.PORT || 5432, (err) => {
   if (err) {
